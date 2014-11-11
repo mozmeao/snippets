fc5fdaf0d1124c23f75a9543988e063cdd1d504d
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-11-11 15:18:56 -0800

    refactoring build process to emit standalone snippet partials and entire demo pages

--- mofo-eoy-2014/gulpfile.js
+++ mofo-eoy-2014/gulpfile.js
@@ -4,25 +4,38 @@ var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 var connect = require('gulp-connect');
 
-gulp.task('build', function() {
+// Construct snippet HTML partials
+gulp.task('build-snippets', function() {
   var css = fs.readFileSync('snippet.css');
   var js = fs.readFileSync('snippet.js');
 
-  gulp.src('index.template')
-    .pipe(rename('index.html'))
-    .pipe(replace('{{ CSS }}', css))
-    .pipe(replace('{{ JS }}', js))
-    .pipe(gulp.dest('./build/'));
-
-  gulp.src('index-alt.template')
-    .pipe(rename('index-alt.html'))
+  return gulp.src(['snippet.template', 'snippet-alt.template'])
+    .pipe(rename(function (path) {
+      path.extname = '.html'
+    }))
     .pipe(replace('{{ CSS }}', css))
     .pipe(replace('{{ JS }}', js))
     .pipe(gulp.dest('./build/'));
 });
 
+// Construct demo html pages
+gulp.task('build-pages', ['build-snippets'], function () {
+  var builtSnippet = fs.readFileSync('build/snippet.html');
+  var builtSnippetAlt = fs.readFileSync('build/snippet-alt.html');
+
+  gulp.src('index.template')
+    .pipe(rename('index.html'))
+    .pipe(replace('{{ SNIPPET }}', builtSnippet))
+    .pipe(gulp.dest('./build/'));
+
+  gulp.src('index.template')
+    .pipe(rename('index-alt.html'))
+    .pipe(replace('{{ SNIPPET }}', builtSnippetAlt))
+    .pipe(gulp.dest('./build/'));
+})
+
 gulp.task('watch', function () {
-  var watcher = gulp.watch(['index.template', 'index-alt.template', 'snippet.js', 'snippet.css'], ['build']);
+  var watcher = gulp.watch(['*.template', 'snippet.js', 'snippet.css'], ['build']);
 });
 
 gulp.task('connect', function() {
@@ -32,4 +45,5 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('build', ['build-pages']);
 gulp.task('default', ['build', 'connect', 'watch']);
